0a90d8ae6e92d31d76717c4833bfcf1c3ebda928
Matthew Pearson <pearsonmr@ornl.gov>
2019-02-13 13:21:10 -0500

    E1214 support: reading back IP address and uptime is working.

--- example/iocBoot/iocexample/st.cmd
+++ example/iocBoot/iocexample/st.cmd
@@ -11,9 +11,20 @@ cd "${TOP}"
 dbLoadDatabase "dbd/example.dbd"
 example_registerRecordDeviceDriver pdbbase
 
+epicsEnvSet("IP_ADDR","192.168.200.177:502")
+epicsEnvSet("IP_PORT","m1ip")
+epicsEnvSet("PORT","m1")
+
+< $(MOXA)/st_scripts/st_E1214.cmd
+
+asynSetTraceMask("m13w",0,0xFF)
+asynSetTraceIOMask("m13w",0,0xFF)
+
 ## Load record instances
 dbLoadRecords "db/example.db"
 
 cd "${TOP}/iocBoot/${IOC}"
 iocInit
+
+
 
--- st_scripts/st_E1214.cmd
+++ st_scripts/st_E1214.cmd
@@ -24,18 +24,20 @@ modbusInterposeConfig("$(IP_PORT)", 0, 2000, 0)
 # arg 9: plcType
 
 # DI Read:
-drvModbusAsynConfigure("$(PORT)dir", "$(IP_PORT)", 1, 4, 48, 1, 0, 200, 0)
+#drvModbusAsynConfigure("$(PORT)dir", "$(IP_PORT)", 1, 4, 48, 1, 0, 200, 0)
 
 # Relay Read/Write:
-drvModbusAsynConfigure("$(PORT)rr", "$(IP_PORT)", 1, 4, 32, 1, 0, 200, 0)
-drvModbusAsynConfigure("$(PORT)rw", "$(IP_PORT)", 1, 1, 0,  6, 0, 200, 0)
+#drvModbusAsynConfigure("$(PORT)rr", "$(IP_PORT)", 1, 4, 32, 1, 0, 200, 0)
+#drvModbusAsynConfigure("$(PORT)rw", "$(IP_PORT)", 1, 1, 0,  6, 0, 200, 0)
 
 # Device Information (used to read strings once at startup using waveforms)
 # Use absolute addressing for this so we are not polling
-drvModbusAsynConfigure("$(PORT)info", "$(IP_PORT)", 1, 4, -1, 30, 0, 200, 0)
+drvModbusAsynConfigure("$(PORT)info", "$(IP_PORT)", 1, 4, -1, 3, 0, 200, 0)
 
 # Device Uptime & Watchdog Read/Write:
-drvModbusAsynConfigure("$(PORT)up", "$(IP_PORT)", 1, 4, 5020, 2, 0, 1000, 0)
-drvModbusAsynConfigure("$(PORT)wr", "$(IP_PORT)", 1, 1, 4140, 1, 0, 1000, 0)
-drvModbusAsynConfigure("$(PORT)ww", "$(IP_PORT)", 1, 5, 4140, 1, 0, 1000, 0)
+#drvModbusAsynConfigure("$(PORT)up", "$(IP_PORT)", 1, 4, 5020, 2, 6, 1000, 0)
+drvModbusAsynConfigure("$(PORT)2w", "$(IP_PORT)", 1, 4, -1, 2, 0, 1000, 0)
+drvModbusAsynConfigure("$(PORT)3w", "$(IP_PORT)", 1, 4, -1, 3, 0, 1000, 0)
+#drvModbusAsynConfigure("$(PORT)wr", "$(IP_PORT)", 1, 1, 4140, 1, 0, 1000, 0)
+#drvModbusAsynConfigure("$(PORT)ww", "$(IP_PORT)", 1, 5, 4140, 1, 0, 1000, 0)
 
