f74fa6fd6a7687a16f15d160951149f8b0396f10
Matthew Pearson <pearsonmr@ornl.gov>
2019-02-14 12:57:57 -0500

    st_E1214.cmd: add single bit read function.

--- st_scripts/st_E1214.cmd
+++ st_scripts/st_E1214.cmd
@@ -23,17 +23,6 @@ modbusInterposeConfig("$(IP_PORT)", 0, 2000, 0)
 # arg 8: pollMsec 
 # arg 9: plcType
 
-# DI Read:
-#drvModbusAsynConfigure("$(PORT)dir", "$(IP_PORT)", 1, 4, 48, 1, 0, 200, 0)
-
-# Relay Read/Write:
-#drvModbusAsynConfigure("$(PORT)rr", "$(IP_PORT)", 1, 4, 32, 1, 0, 200, 0)
-#drvModbusAsynConfigure("$(PORT)rw", "$(IP_PORT)", 1, 1, 0,  6, 0, 200, 0)
-
-# Device Information (used to read strings once at startup using waveforms)
-# Use absolute addressing for this so we are not polling
-drvModbusAsynConfigure("$(PORT)info", "$(IP_PORT)", 1, 4, -1, 3, 0, 200, 0)
-
 # Use separate asyn ports for each length of read. The device gives us a modbus error
 # if we try to readout outside of the defined address space, but the information
 # isn't nicely ordered in address space so we need to cherry pick different locations
@@ -47,3 +36,8 @@ drvModbusAsynConfigure("$(PORT)2w",  "$(IP_PORT)", 1, 4, -1, 2,  0, 1000, 0)
 drvModbusAsynConfigure("$(PORT)3w",  "$(IP_PORT)", 1, 4, -1, 3,  0, 1000, 0)
 drvModbusAsynConfigure("$(PORT)10w", "$(IP_PORT)", 1, 4, -1, 10, 0, 1000, 0)
 drvModbusAsynConfigure("$(PORT)30w", "$(IP_PORT)", 1, 4, -1, 30, 0, 1000, 0)
+
+# Single bit reading 
+drvModbusAsynConfigure("$(PORT)1b",  "$(IP_PORT)", 1, 1, -1, 1,  0, 1000, 0) 
+
+epicsThreadSleep(1)
