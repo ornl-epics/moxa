87c861a0bf3374764fab79f9e68b14ddee5c42f7
Matthew Pearson <pearsonmr@ornl.gov>
2019-02-22 15:02:09 -0500

    example: include 3 units at the same time.

--- example/iocBoot/iocexample/st.cmd
+++ example/iocBoot/iocexample/st.cmd
@@ -12,20 +12,21 @@ dbLoadDatabase "dbd/example.dbd"
 example_registerRecordDeviceDriver pdbbase
 
 #E1214 Unit (6 DI and 6 Relay)
-#epicsEnvSet("IP_ADDR","192.168.200.177:502")
-#epicsEnvSet("IP_PORT","m1ip")
-#epicsEnvSet("PORT","m1")
+epicsEnvSet("IP_ADDR","192.168.200.177:502")
+epicsEnvSet("IP_PORT","m1ip")
+epicsEnvSet("PORT","m1")
+< $(MOXA)/st_scripts/st_common.cmd
 
 #E1241 Unit (4 AO)
-#epicsEnvSet("IP_ADDR","192.168.200.178:502")
-#epicsEnvSet("IP_PORT","m2ip")
-#epicsEnvSet("PORT","m2")
+epicsEnvSet("IP_ADDR","192.168.200.178:502")
+epicsEnvSet("IP_PORT","m2ip")
+epicsEnvSet("PORT","m2")
+< $(MOXA)/st_scripts/st_common.cmd
 
 #E1241 Unit (4 DI, 4 DIO, 4 AI)
 epicsEnvSet("IP_ADDR","192.168.200.179:502")
 epicsEnvSet("IP_PORT","m3ip")
 epicsEnvSet("PORT","m3")
-
 < $(MOXA)/st_scripts/st_common.cmd
 
 #asynSetTraceMask("m11w4",48,0xFF)
